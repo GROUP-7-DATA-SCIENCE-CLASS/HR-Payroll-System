7b20ee17149d7ec6e6487ad68c4b9968f1c3ee02
jasminemutagamba <jasminemuts5@gmail.com>
2026-09-15 19:11:43 +0300

    add tax calculation

--- HR-Payroll-System-file.py
+++ HR-Payroll-System-file.py
@@ -3,3 +3,16 @@
 # ---------------------------------------------
 def calculate_gross_pay(self):
     return self.calculate_regular_pay() + self.calculate_overtime_pay()
+
+# ---------------------------------------------
+# Tax
+# ---------------------------------------------
+def calculate_tax(self):
+    gross_pay = self.calculate_gross_pay()
+
+    if gross_pay <= TAX_THRESHOLD:
+        return 0
+
+    taxable_amount = gross_pay - TAX_THRESHOLD
+    return taxable_amount * TAX_RATE
+
