6110c5bd7a925103b46569c12ea74d8925103b7f
NDAMBIRE HARRY BOSCO HAQQAA <babyharry17@gmail.com>
2026-09-15 18:12:50 +0300

    add validation method for objects created

---
+++ HR-Payroll-System-file.py
@@ -0,0 +1,12 @@
+ # ---------------------------------------------
+    # Validation Method
+    # ---------------------------------------------
+def validate_inputs(self):
+        if not self.name.strip():
+            raise ValueError("Employee name cannot be empty.")
+
+        if self.hours_worked < 0:
+            raise ValueError("Hours worked cannot be negative.")
+
+        if self.hourly_rate <= 0:
+            raise ValueError("Hourly rate must be greater than zero.")
