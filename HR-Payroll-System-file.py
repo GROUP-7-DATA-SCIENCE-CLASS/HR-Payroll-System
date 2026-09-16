29762b7105204cb399b218b1275b7583b3860a60
Conrad Natuyamba <237318190+ConradNats@users.noreply.github.com>
2026-09-16 12:18:08 +0300

    Making the add employees function

--- HR-Payroll-System-file.py
+++ HR-Payroll-System-file.py
@@ -9,3 +9,17 @@ class PayrollSystem:
 
     def _init_(self):
         self.employees = []
+# ---------------------------------------------
+    # Add Employee
+    # ---------------------------------------------
+    def add_employee(self, employee):
+
+        # Check duplicate employee number
+        for emp in self.employees:
+            if emp.employee_number == employee.employee_number:
+                raise ValueError(
+                    f"Employee number {employee.employee_number} already exists."
+                )
+
+        self.employees.append(employee)
+        print(f"Employee '{employee.name}' added successfully.")
