8f6a70d9894d9748454cabb6a8ad981aff584b5b
Conrad Natuyamba <237318190+ConradNats@users.noreply.github.com>
2026-09-16 12:15:34 +0300

    Making the Payroll Class and add employee list

---
+++ HR-Payroll-System-file.py
@@ -0,0 +1,11 @@
+# =====================================================
+# PAYROLL SYSTEM CLASS
+# =====================================================
+
+class PayrollSystem:
+    """
+    Stores and manages multiple employees.
+    """
+
+    def _init_(self):
+        self.employees = []
